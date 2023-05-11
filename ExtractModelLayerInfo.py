75487fde4d78df8925ee0a060af4f691679c66ca
Chingwo Cheung <cheung59@gilbreth-fe02.rcac.purdue.edu>
2023-05-11 16:44:02 -0400

    Add ONNX architecture printing

--- ExtractModelLayerInfo.py
+++ ExtractModelLayerInfo.py
@@ -2,11 +2,29 @@ from transformers import AutoImageProcessor, ResNetForImageClassification
 import torch
 from datasets import load_dataset
 from torchvision.models import resnet50, ResNet50_Weights
-import LayerClass
+import onnx
+from GetOnnxFileFromURL import getModel
 
 #processor = AutoImageProcessor.from_pretrained("microsoft/resnet-50")
 model1 = ResNetForImageClassification.from_pretrained("microsoft/resnet-18")
 model2 = torch.hub.load('pytorch/vision:v0.10.0', 'resnet18', pretrained=True)
+model3 = onnx.load('/depot/davisjam/data/chingwo/PTM-Naming/resnet18-v2-7.onnx')
+
+graph = model3.graph
+
+initializers = {init.name: init for init in graph.initializer}
+
+shapes = {vi.name: [dim.dim_value for dim in vi.type.tensor_type.shape.dim] for vi in graph.value_info}
+
+prevname_bn = False
+
+for i, node in enumerate(graph.node):
+    for name in node.input:
+        if name in initializers:
+            tensor = initializers[name]
+            if not ('batchnorm' in name and 'gamma' not in name and 'beta' not in name):
+                print(name, tensor.dims)
+
 
 np1 = list(model1.named_parameters())
 np2 = list(model2.named_parameters())
@@ -14,6 +32,4 @@ np2 = list(model2.named_parameters())
 for i in range(len(np1)):
     print(np1[i][0], np2[i][0])
     print(np1[i][1].size(), np2[i][1].size())
-'''
-l = LayerClass.LayerTools.CreateFromString('Conv2d(2048, 512, kernel_size=(1, 1), stride=(1, 1), bias=False)')
-print(l)'''
+
