8badbc76ecf403e01a8a5a00575f725cdf80b384
Chingwo Cheung <cheung59@gilbreth-fe01.rcac.purdue.edu>
2023-06-22 15:40:29 -0400

    changed file name and added tests

---
+++ comparators/pytorch/test_comp.py
@@ -0,0 +1,16 @@
+
+from transformers import ResNetForImageClassification
+from PyTorchModelComparator import OrderedListComparator
+from utils import print_list
+import timm
+import torch
+
+m1 = ResNetForImageClassification.from_pretrained("microsoft/resnet-50")
+m2 = timm.create_model('resnet50.a1_in1k', pretrained=True)
+#m2.eval()
+comp = OrderedListComparator()
+comp.from_model(m1, m2, torch.zeros(1, 3, 224, 224))
+print_list(comp.l1)
+print('======')
+print_list(comp.l2)
+print(comp.get_sim_metric())
