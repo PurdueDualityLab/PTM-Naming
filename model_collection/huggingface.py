9f8fe3bc729c71ea9c6f9e898371d60e8a2c6a8d
Wenxin-Jiang <wenxin_jiang@outlook.com>
2023-07-05 09:17:41 +0000

    filtered_models and downloads

--- model_collection/huggingface.py
+++ model_collection/huggingface.py
@@ -1,6 +1,8 @@
 import os
 import sys
 import json
+from tqdm import tqdm
+
 import transformers
 from huggingface_hub import hf_hub_download, HfApi, ModelFilter, list_models
 
@@ -30,63 +32,54 @@ def load_model_list(path='./modelArch_list.json') -> list:
     return model_list
 
 
-def get_downloads(model_name):
-    # TODO: the search is weird, need to fix it.
-    
+def get_downloads(model_name):    
     # Use hfapi to get the model from model_name)
-    model_name = model_name.split('/')
-    if len(model_name) > 1:
-        author = model_name[0]
-        model_name = model_name[1]
-        model_filter = ModelFilter(
-            author=author,
-            model_name=model_name,
-        )
-    else:
-        model_filter = ModelFilter(
-            model_name=model_name[0],
-        )
-
-    models = api.list_models(filter=model_filter)
-    # models = api.list_models(model_args["model_name"]=model_name)
-    
-    cnt = 0
-    for model in models:
-        # Use hfapi to get the model from model_name)
-        downloads=model.downloads
-        cnt += 1
-    if cnt > 1:
-        logger.error(f'More than 1 model for {model_name}')
-        return downloads
-    else:
-        return downloads
+    try:
+        return api.model_info(repo_id=model_name).downloads
+    except:
+        return None
+        
 
 def get_model_from_arch(model_arch: str, metadata_dict: dict):
     '''
     Get all model from huggingface with the same model architecture
     The models are obtained from model metadata.
     ''' 
+    
+
     if model_arch not in metadata_dict:
         logger.debug(f'No model for {model_arch}, continue to next model.')
     else:
         model_metadata = metadata_dict[model_arch]
+        model_metadata_nameDownload = model_metadata.copy()
+        
         # Get the model downloads
         for model in model_metadata:
             model_download = get_downloads(model)
-            if model_download < 10:
-                logger.info(f'{model} has less than 10 downloads, continue to next model.')
+            if model_download is None:
+                logger.debug(f'{model} does not exist, continue to next model.')
                 model_metadata.remove(model)
-        logger.info(f'Loading {len(model_metadata)} {model_arch} models.')
+                model_metadata_nameDownload.remove(model)
+            elif model_download < 10:
+                logger.debug(f'{model} has less than 10 downloads, continue to next model.')
+                model_metadata.remove(model)
+                model_metadata_nameDownload.remove(model)
+            else:
+                # replace mdoel with (model, model_download) in model_metadata
+                model_metadata_nameDownload[model_metadata.index(model)] = (model, model_download)
+        logger.debug(f'Loading {len(model_metadata)} {model_arch} models.')
         # Saving the model_archs with more than 2 models
         if len(model_metadata) > 1:
-            return model_metadata
+            return model_metadata, model_metadata_nameDownload
         else:
-            logger.info(f'Only 1 model for {model_arch}, continue to next model.')
-    return None
+            logger.debug(f'Only 1 model for {model_arch}, continue to next model.')
+    return None, None
 
 
 def filter_downloads(filtered_models: dict) -> dict:
-
+    '''
+        TODO: Remove. Use hf_download instead
+    '''
     # Load filtered_models
     with open(os.path.join(os.path.dirname(__file__), 'filtered_models.json'), "r") as f:
         filtered_models = json.load(f)
@@ -97,23 +90,41 @@ def filter_downloads(filtered_models: dict) -> dict:
             sys.exit()
     return filtered_models
 
+
 def load_model(model_list: list, metadata_dict: dict):
     '''
     Load model with same architecture from huggingface
     '''
     filtered_models = {}
-    for model_arch in model_list:
-        models_metadata = get_model_from_arch(model_arch, metadata_dict)
+    filtered_models_nameDownload = {}
+
+    for model_arch in tqdm(model_list):
+         
+        models_metadata, model_metadata_nameDownload = get_model_from_arch(model_arch, metadata_dict)
+       
     
         if models_metadata:
-            logger.info(f'Saving {model_arch} to filtered_models')
+            logger.debug(f'Saving {model_arch} to filtered_models_nameOnly')
             filtered_models[model_arch] = models_metadata
         
+        if model_metadata_nameDownload:
+            logger.debug(f'Saving {model_arch} to filtered_models_nameDownload')
+            filtered_models_nameDownload[model_arch] = model_metadata_nameDownload
+        
     logger.info(f'Number of filtered_models: {len(filtered_models)}')
-    filtered_models = filter_downloads(filtered_models)
-    logger.success(f'Saving filtered_models to filtered_models.json')
+    logger.info(f'Number of filtered_models_nameDownload: {len(filtered_models_nameDownload)}')
+    # filtered_models = filter_downloads(filtered_models)
+    
     with open(os.path.join(os.path.dirname(__file__), 'filtered_models.json'), 'w') as f:
         json.dump(filtered_models, f, indent=4)
+    logger.success(f'Saved filtered_models!')
+
+    with open(os.path.join(os.path.dirname(__file__), 'filtered_models_nameDownload.json'), 'w') as f:
+        json.dump(filtered_models_nameDownload, f, indent=4)
+    logger.success(f'Saved filtered_models_nameDownload!')
+
+
+
     return
 
 
