8abdeca482f615597affcb47b8da83627a50a9b2
Chingwo Cheung <cheung59@gilbreth-fe03.rcac.purdue.edu>
2023-09-01 16:09:31 -0400

    Added some comments

--- vectorizer/pytorch/onnx_vectorize_example.py
+++ vectorizer/pytorch/onnx_vectorize_example.py
@@ -1,12 +1,15 @@
 from onnx_vectorize import vectorize
 
-file_path = '/depot/davisjam/data/chingwo/PTM-Naming/models/resnet18-v1-7.onnx'
+if __name__ == '__main__':
 
-d, l, p = vectorize(file_path)
+    # Enter your own file path
+    file_path = '/depot/davisjam/data/chingwo/PTM-Naming/models/resnet18-v1-7.onnx'
 
-print('Dimension Vector')
-print(d)
-print('Layer Vector')
-print(l)
-print('Parameter Vector')
-print(p)
+    d, l, p = vectorize(file_path)
+
+    print('Dimension Vector')
+    print(d)
+    print('Layer Vector')
+    print(l)
+    print('Parameter Vector')
+    print(p)
