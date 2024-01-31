c788089f0cb1f7989f99e2ca1d27a6ad0d0ca657
cwc020730 <xxzhangjinghexx@gmail.com>
2024-01-31 02:02:45 -0500

    add a __init__.py

---
+++ __init__.py
@@ -0,0 +1,17 @@
+from ANN import (
+    AbstractNN,
+    AbstractNNLayer,
+    AbstractNNLayerParam,
+    AbstractNNGenerator,
+    AbstractNNConversionHandler,
+    AbstractNNSorter
+)
+
+__all__ = [
+    AbstractNN,
+    AbstractNNLayer,
+    AbstractNNLayerParam,
+    AbstractNNGenerator,
+    AbstractNNConversionHandler,
+    AbstractNNSorter
+]
