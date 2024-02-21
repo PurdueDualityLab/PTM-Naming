ab7ee505e36a6f10c33189bbb32cde77b5130b6c
cwc020730 <xxzhangjinghexx@gmail.com>
2024-02-20 20:59:00 -0500

    add code for processing random samples

---
+++ data_files/scripts/convert_rand_sample_peatmoss_models.py
@@ -0,0 +1,53 @@
+"""
+This script is used to convert the random sample of peatmoss models to a vectorized format.
+"""
+
+import json
+import subprocess
+import time
+import os
+from dotenv import load_dotenv
+from loguru import logger
+
+def run_process():
+    """Run the main application process."""
+
+    load_dotenv(".env")
+    module_use_path = str(os.getenv("MODULE_USE_PATH"))
+    module_load_path = str(os.getenv("MODULE_LOAD_PATH"))
+    python_path = str(os.getenv("PYTHONPATH"))
+    if not os.path.exists(f"{os.getenv('PEATMOSS_VEC_DATA_PATH')}/rand_sample"):
+        os.makedirs(f"{os.getenv('PEATMOSS_VEC_DATA_PATH')}/rand_sample", exist_ok=True)
+    json_file_loc = f"{os.getenv('PEATMOSS_VEC_DATA_PATH')}/rand_sample"
+    model_list_json_loc = "data_files/json_files/selected_peatmoss_repos.json"
+    if not os.path.exists(model_list_json_loc):
+        raise FileNotFoundError(f"File {model_list_json_loc} does not exist.")
+
+    command = f"""
+    module use {module_use_path} && 
+    module load {module_load_path} && 
+    export PYTHONPATH=$PYTHONPATH:{python_path} &&
+    python data_files/scripts/export_vec.py -j {json_file_loc} -l {model_list_json_loc}
+    """
+
+    return subprocess.Popen(command, shell=True, executable='/bin/bash')
+
+def monitor_process(p: subprocess.Popen) -> int:
+    """Monitor the main application process."""
+
+    while True:
+        result = p.poll()
+        if result is not None:  # Process has exited
+            logger.info(f"Process has exited with {result}.")
+            return result
+        time.sleep(0.1)
+
+if __name__ == "__main__":
+    idx = -1
+    while idx < 1000:
+        p = run_process()
+        res = monitor_process(p)
+        with open("data_files/other_files/temp_index.txt", "r", encoding="utf-8") as f:
+            idx = int(f.readline())
+        logger.info(f"Index: {idx}.")
+        
--- data_files/scripts/export_vec.py
+++ data_files/scripts/export_vec.py
@@ -2,6 +2,7 @@
 This script exports a single vector from a hf repository.
 """
 
+import json
 import os
 from pyexpat import model
 import sys
@@ -11,40 +12,39 @@ from dotenv import load_dotenv
 from ANN.abstract_neural_network import AbstractNN
 
 if __name__ == "__main__":
-    if len(sys.argv) != 6:
+    if len(sys.argv) != 5:
         print("Invalid number of arguments.")
         sys.exit(1)
     elif sys.argv[1] == "help":
         print("This script exports vectors from a hf repository.")
-        print("Usage: python export_vec.py -r <repo_name> -j <json_output_loc> -l <model_list_json_loc>")
+        print("Usage: python export_vec.py -j <json_output_loc> -l <model_list_json_loc>")
         sys.exit(0)
-    elif sys.argv[1] != "-r":
-        print("Invalid argument.")
-        sys.exit(1)
-    elif sys.argv[3] != "-j":
-        print("Invalid argument.")
-        sys.exit(1)
-    elif sys.argv[5] != "-l":
-        print("Invalid argument.")
+    elif sys.argv[1] != "-j" or sys.argv[3] != "-l":
+        print("Invalid arguments.")
         sys.exit(1)
     else:
-        repo_name = sys.argv[2]
-        json_output_loc = sys.argv[4]
-        model_list_json_loc = sys.argv[6]
+        json_output_loc = sys.argv[2]
+        model_list_json_loc = sys.argv[4]
     with open(model_list_json_loc, "r", encoding="utf-8") as f:
-        model_list = f.read()
+        model_list = json.load(f)
     with open("data_files/other_files/temp_index.txt", "r", encoding="utf-8") as f:
-        idx = f.readline()
+        idx = int(f.readline())
     logger.info(f"Index: {idx}.")
-    model_list = model_list[int(idx):]
+    model_list = model_list[idx:]
     for i, repo_name in enumerate(model_list):
+        idx += 1
         with open("data_files/other_files/temp_index.txt", "w", encoding="utf-8") as f:
             f.seek(0)
-            f.writelines(str(i))
+            f.writelines(str(idx))
         logger.info(f"[{i}] Processing {repo_name}.")
         try:
             ann = AbstractNN.from_huggingface(repo_name)
-            ann.export_vector(json_output_loc)
+            if os.path.exists(json_output_loc + f"/{repo_name}.json"):
+                logger.info(f"File {repo_name}.json already exists.")
+                continue
+            else:
+                os.makedirs(json_output_loc, exist_ok=True)
+            ann.export_vector(json_output_loc + f"/{repo_name}.json")
         except Exception as emsg: # pylint: disable=broad-except
             logger.error(emsg)
             sys.exit(1)
