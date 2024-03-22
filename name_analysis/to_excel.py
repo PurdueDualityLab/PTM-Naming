c89dd3e04554ba6cfd3c6bf6a82cbbaa5d470735
cwc020730 <xxzhangjinghexx@gmail.com>
2024-03-22 17:52:52 -0400

    Updated formatting

--- name_analysis/to_excel.py
+++ name_analysis/to_excel.py
@@ -12,13 +12,13 @@ if __name__ == "__main__":
     print(f"Length of results: {len(results)}")
 
     # The results are in the format {model_name: category_set}
-    
+
     # create a df table with columns as the categories
     df = pd.DataFrame(columns=CATEGORIES)
     for model_full_name, category_set in results.items():
         # create a row with 1s and 0s
         row = [1 if category in category_set else 0 for category in CATEGORIES]
         df.loc[model_full_name] = row
-    
+
     # save the dataframe to an excel file
     df.to_excel('name_analysis/results.xlsx')
