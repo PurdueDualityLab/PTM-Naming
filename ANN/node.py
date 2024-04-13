b821e03275902319a6e34ef7ac24682a99880738
cwc020730 <xxzhangjinghexx@gmail.com>
2024-04-13 19:22:09 -0400

    Finished adding the utility of handling Torch Functions

--- ANN/node.py
+++ ANN/node.py
@@ -13,20 +13,25 @@ class Node:
             self.children.append(child)
         if self not in child.parents:
             child.parents.append(self)
-
+    
+    def __repr__(self):
+        return f'<Node [{self.id}]> -> {[child.id for child in self.children]}'
+    
 class TensorNode(Node):
     def __init__(self, tensor):
         super().__init__()
         self.tensor = tensor
         self.shape = tensor.shape
 
+    def __repr__(self):
+        return f'<TensorNode [{self.id}] shape={self.shape}> -> {[child.id for child in self.children]}'
+
 class FunctionNode(Node):
     def __init__(self, operation):
         super().__init__()
         self.operation = operation
+        self.contained_in_module = False
+        self.module_info = None
 
-class ModuleNode(Node):
-    def __init__(self, module):
-        super().__init__()
-        self.module = module
-        self.module_type = str(type(module))
+    def __repr__(self):
+        return f'<FunctionNode [{self.id}] operation={self.operation} module={self.module_info}> -> {[child.id for child in self.children]}'
--- ANN/tracer.py
+++ ANN/tracer.py
@@ -1,20 +1,9 @@
 import torch
 import torch.nn as nn
+from node import Node, TensorNode, FunctionNode
 
 class TracerTensor(torch.Tensor):
-    _next_id = 0
-
-    def assign_id(self):
-        self.id = TracerTensor._next_id
-        TracerTensor._next_id += 1
-
-    def var_init(self):
-        if not hasattr(self, 'id'):
-            self.assign_id()
-        if not hasattr(self, 'creation_op'):
-            self.creation_op = None
-        if not hasattr(self, 'inputs'):
-            self.inputs = []
+    _next_id = 0  # Class variable to keep track of the next available ID
 
     @staticmethod
     def __new__(cls, x, *args, **kwargs):
@@ -24,49 +13,74 @@ class TracerTensor(torch.Tensor):
         instance.var_init()
         return instance
 
-class TracerModule():
+    def var_init(self):
+        self.assign_id()
+        if not hasattr(self, 'node'):
+            self.node = TensorNode(self)
+        if not hasattr(self, 'creation_op'):
+            self.creation_op = None
+        if not hasattr(self, 'inputs'):
+            self.inputs = []
 
+    def assign_id(self):
+        if not hasattr(self, 'id'):
+            self.id = TracerTensor._next_id
+            TracerTensor._next_id += 1
+
+    @classmethod
+    def __torch_function__(cls, func, types, args=(), kwargs=None):
+        if kwargs is None:
+            kwargs = {}
+        ret = super().__torch_function__(func, types, args, kwargs)
+        if isinstance(ret, torch.Tensor):
+            ret = TracerTensor(ret) if not isinstance(ret, TracerTensor) else ret
+            func_node = FunctionNode(func.__name__)
+            input_nodes = [arg.node for arg in args if isinstance(arg, TracerTensor)]
+            for node in input_nodes:
+                node.add_child(func_node)
+            if isinstance(ret, TracerTensor):
+                ret.node = TensorNode(ret)
+                func_node.add_child(ret.node)
+        return ret
+
+class TracerModule():
     def __init__(self, module):
         self.module = module
-        self.input_tracer_tensor = []
-        self.output_tracer_tensor = None
-        self.visited = False
+        #self.module_node = ModuleNode(module)  # Represents the module
+        self.wrap_forward()
 
     def wrap_forward(self):
         original_forward = self.module.forward
         
         def wrapped_forward(*inputs, **kwargs):
-            self.visited = True
-            # new_inputs = [TracerTensor(inp) if not isinstance(inp, TracerTensor) else inp for inp in inputs]
-            new_inputs = []
-            for inp in inputs:
-                if not isinstance(inp, TracerTensor):
-                    inp = TracerTensor(inp)
-                inp.var_init()
-                new_inputs.append(inp)
-            self.input_tracer_tensor = new_inputs
-            new_inputs_clone = [inp.clone() for inp in new_inputs] # avoid modifying the original input
-            output = original_forward(*new_inputs_clone, **kwargs)
+            new_inputs = [TracerTensor(inp) if not isinstance(inp, TracerTensor) else inp for inp in inputs]
+            output = original_forward(*new_inputs, **kwargs)
             if not isinstance(output, TracerTensor):
                 output = TracerTensor(output)
             output.var_init()
-            self.output_tracer_tensor = output
+            # Link nodes
+            # for inp in new_inputs:
+            #     inp.node.add_child(self.module_node)
+            # self.module_node.add_child(output.node)
+            for inp in new_inputs:
+                for child in inp.node.children:
+                    child.contained_in_module = True
+                    child.module_info = self.module
             return output
 
         self.module.forward = wrapped_forward
 
 class Tracer:
-
     def __init__(self, model):
         self.model = model
         self.tracer_module_list = []
+        self.input_tensor = None
         self.wrap_modules(model)
 
     def wrap_modules(self, module):
         if not list(module.children()):
             tracer_module = TracerModule(module)
             self.tracer_module_list.append(tracer_module)
-            tracer_module.wrap_forward()
         else:
             for child in module.children():
                 self.wrap_modules(child)
@@ -74,6 +88,7 @@ class Tracer:
     def trace(self, input_tensor):
         if not isinstance(input_tensor, TracerTensor):
             input_tensor = TracerTensor(input_tensor)
+        self.input_tensor = input_tensor
         output = self.model(input_tensor)
         return output
 
@@ -84,5 +99,16 @@ tracer = Tracer(model)
 dummy_input = torch.randn(1, 3, 224, 224)
 output = tracer.trace(dummy_input)
 
-for tracer_module in tracer.tracer_module_list:
-    print(f"Module: {tracer_module.module}, Input: {[inp.id for inp in tracer_module.input_tracer_tensor]}, Output: {tracer_module.output_tracer_tensor.id}")
+# traverse the graph and print the nodes
+visited = set()
+def traverse(node):
+    if node in visited:
+        return
+    visited.add(node)
+    for child in node.children:
+        traverse(child)
+
+traverse(tracer.input_tensor.node)
+visited = sorted(list(visited), key=lambda x: x.id) # Sort the nodes by ID
+for node in visited:
+    print(node)
