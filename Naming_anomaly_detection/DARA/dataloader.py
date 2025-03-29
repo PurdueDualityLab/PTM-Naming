0b69572434f2ec3c1910804ea6d2ead7577b3dcc
Mingyu Kim <calixkim58@gmail.com>
2025-03-29 19:53:13 -0400

    multi-label option for task label

--- Naming_anomaly_detection/DARA/dataloader.py
+++ Naming_anomaly_detection/DARA/dataloader.py
@@ -1,83 +1,69 @@
 import json
 import random
 import torch
+from torch.utils.data import Dataset
+from sklearn.preprocessing import MultiLabelBinarizer
 
-class DARA_dataset:
-    def __init__(self, dict_path: str, label_type: str) -> None:
+class DARA_dataset(Dataset):
+    def __init__(self, dict_path: str, label_type: str, train_ratio: float = 0.8, multi_label=False) -> None:
         self.label_type = label_type
-        self.model_dict = self.load_dict(dict_path)  # Load model vectors
-        self.model_names = list(self.model_dict.keys())  # List of model names
-        self.num_classes = len(set(self.model_dict[model_name]['arch'] for model_name in self.model_names))  # Number of unique classes
-        self.data = self.processing()  # Merge vectors with labels
-        self.label_to_index = self.create_label_mapping()  # Create label mapping
-        self.index_to_label = {idx: label for label, idx in self.label_to_index.items()}  # Create index to label mapping
-
-        self.convert_labels_to_indices()  # Convert labels to indices
-        self.shuffle_data()  # Shuffle data for training
-        self.train_data, self.test_data = self.split_data()  # Split data into training and testing sets
+        self.multi_label = multi_label
+        self.model_dict = self.load_dict(dict_path)
+        self.model_names = list(self.model_dict.keys())
+        self.data = self.processing()
+        self.mlb = MultiLabelBinarizer()
+        self.mlb.fit([item[1] for item in self.data])
+        self.label_to_index = {label: idx for idx, label in enumerate(self.mlb.classes_)}
+        self.index_to_label = {idx: label for label, idx in self.label_to_index.items()}
+        if multi_label:
+            self.convert_labels_to_binary()
+        else:
+            self.convert_labels_to_indices()
+        self.shuffle_data()
 
     def load_dict(self, dict_path: str) -> dict:
         with open(dict_path, 'r') as f:
-            model_dict = json.load(f)
-        return model_dict
-
+            return json.load(f)
 
     def processing(self) -> list:
         data = []
         for model_name in self.model_names:
-            '''
-            TODO: Check different weights and settings of the vectors.
-            '''
-            l_tensor = torch.tensor(self.model_dict[model_name]['l'], dtype=torch.float).unsqueeze(0)  # Add an extra dimension
-            p_tensor = (1 * torch.tensor(self.model_dict[model_name]['p'], dtype=torch.float)).unsqueeze(0)  # Scale and add an extra dimension
-            vec = torch.cat((l_tensor, p_tensor), dim=1)  # Concatenate along the new dimension
-
-            # torch.tensor(self.model_dict[model_name]['l'], dtype=torch.float) + 0.5*torch.tensor(self.model_dict[model_name]['p'], dtype=torch.float)
+            l_tensor = torch.tensor(self.model_dict[model_name]['l'], dtype=torch.float).unsqueeze(0)
+            p_tensor = (1 * torch.tensor(self.model_dict[model_name]['p'], dtype=torch.float)).unsqueeze(0)
+            vec = torch.cat((l_tensor, p_tensor), dim=1)
             label = self.model_dict[model_name][self.label_type]
+            if self.multi_label and not isinstance(label, list):
+                label = [label]  # Ensure labels are always lists
             data.append((vec, label, model_name))
         return data
 
-    def create_label_mapping(self):
-        unique_labels = sorted(set(label for _, label, _ in self.data))
-        return {label: idx for idx, label in enumerate(unique_labels)}
-
+    def convert_labels_to_binary(self):
+        new_data = []
+        for vec, labels, name in self.data:
+            binary_labels = self.mlb.transform([labels])[0].tolist()
+            new_data.append((vec, binary_labels, name))
+        self.data = new_data
+    
     def convert_labels_to_indices(self):
-        self.data = [(vec, self.label_to_index[label], name) for vec, label, name in self.data]
+        self.data = [(vec, self.label_to_index[label[0]], name) for vec, label, name in self.data]
+
+                
 
     def shuffle_data(self):
         random.shuffle(self.data)
 
-    def split_data(self, train_ratio: float = 0.8):
-        split_index = int(len(self.data) * train_ratio)
-        train_data = self.data[:split_index]
-        test_data = self.data[split_index:]
-        return train_data, test_data
-
-
     def __getitem__(self, idx):
-        # Get the data sample (features and label) for the given index
         vec, label, name = self.data[idx]  # Adjust if your data structure is different
-        return vec, label, name
-
+        return vec, torch.tensor(label, dtype=torch.float), name
+    
     def __len__(self):
         return len(self.data)
 
-    
-    def train_len(self):
-        return len(self.train_data)
-
-    
-    def test_len(self):
-        return len(self.test_data)
-
-    
     def get_num_classes(self):
-        return self.num_classes
-
+        return len(self.mlb.classes_)
 
     def get_label_mapping(self):
         return self.index_to_label
-
 
     def get_data_shape(self):
         return self.data[0][0].size()
--- Naming_anomaly_detection/DARA/run.py
+++ Naming_anomaly_detection/DARA/run.py
@@ -7,10 +7,10 @@ import torch.nn as nn
 import torch.optim as optim
 from torch.optim.lr_scheduler import StepLR
 from torch.utils.data import DataLoader
-from sklearn.metrics import confusion_matrix, ConfusionMatrixDisplay
+from sklearn.metrics import confusion_matrix, ConfusionMatrixDisplay, hamming_loss, accuracy_score
+import seaborn as sns
 
 import matplotlib.pyplot as plt
-from loguru import logger
 
 from modeling import DARA_classifier
 from dataloader import DARA_dataset
@@ -32,7 +32,7 @@ def set_seed(seed=42):
     torch.backends.cudnn.deterministic = True
     torch.backends.cudnn.benchmark = False
 
-def train_and_evaluate(model, train_loader, eval_loader, criterion, optimizer, scheduler, device="cuda", epochs=10, eval_interval=10):
+def train_and_evaluate(model, train_loader, eval_loader, criterion, optimizer, scheduler, device="cuda", epochs=10, eval_interval=10, multi_label=False):
     model.train()  # Set model to training mode
     train_losses = []
     eval_accuracies = []
@@ -42,7 +42,7 @@ def train_and_evaluate(model, train_loader, eval_loader, criterion, optimizer, s
     for epoch in range(epochs):
         total_loss = 0
         for data, target, _ in train_loader:  # Assuming data loaders yield (data, target) tuples
-            data, target = data.to(device), target.to(device).long()  # Ensure target is the correct type for loss calculation
+            data, target = data.to(device), target.to(device)  # Ensure target is the correct type for loss calculation
             optimizer.zero_grad()  # Clear gradients
             output = model(data)  # Forward pass
             loss = criterion(output, target)  # Compute loss
@@ -58,7 +58,7 @@ def train_and_evaluate(model, train_loader, eval_loader, criterion, optimizer, s
         # Evaluation phase
         if (epoch + 1) % eval_interval == 0 or epoch == epochs - 1:
             model.eval()  # Set model to evaluation mode
-            accuracy, preds, targets = eval(model, eval_loader, device)  # Evaluate model
+            accuracy, preds, targets = eval(model, eval_loader, device, multi_label)  # Evaluate model
             eval_accuracies.append(accuracy)
             all_preds.extend(preds)  # Collect all predictions
             all_targets.extend(targets)  # Collect all true labels
@@ -72,22 +72,62 @@ def train_and_evaluate(model, train_loader, eval_loader, criterion, optimizer, s
 
 
 
-def eval(model, eval_loader, device="cuda"):
-    model.eval()  # Set the model to evaluation mode
+# def eval(model, eval_loader, device="cuda"):
+#     model.eval()  # Set the model to evaluation mode
+#     all_preds = []
+#     all_targets = []
+#     with torch.no_grad():  # No need to track gradients for evaluation
+#         for data, target, _ in eval_loader:  # Change this line to match the expected output
+#             data, target = data.to(device), target.to(device).long()
+#             output = model(data)
+#             _, predicted = torch.max(output, 1)
+#             all_preds.extend(predicted.cpu().numpy())
+#             all_targets.extend(target.cpu().numpy())
+#     correct = (np.array(all_preds) == np.array(all_targets)).sum()
+#     total = len(all_targets)
+#     accuracy = 100 * correct / total
+#     return accuracy, all_preds, all_targets
+
+def top_2_accuracy(y_true, y_logits):
+    top_2 = np.argsort(y_logits, axis=1)[:, -2:]  # Get indices of top 2 predictions
+    accuracy = 0
+    
+    for i in range(len(y_true)):
+        true_labels = np.where(y_true[i] == 1)[0]  # Get indices of true labels
+        for label in true_labels:
+            if label in top_2[i]:
+                accuracy += 1
+                break  # Count a sample as correct if any true label is in top 2
+    return accuracy / len(y_true)
+
+def eval(model, eval_loader, device="cuda", multi_label=False):
+    model.eval()
     all_preds = []
     all_targets = []
-    with torch.no_grad():  # No need to track gradients for evaluation
-        for data, target, _ in eval_loader:  # Change this line to match the expected output
-            data, target = data.to(device), target.to(device).long()
+    with torch.no_grad():
+        for data, target, _ in eval_loader:
+            data, target = data.to(device), target.to(device)
             output = model(data)
-            _, predicted = torch.max(output, 1)
-            all_preds.extend(predicted.cpu().numpy())
-            all_targets.extend(target.cpu().numpy())
-    correct = (np.array(all_preds) == np.array(all_targets)).sum()
-    total = len(all_targets)
-    accuracy = 100 * correct / total
-    return accuracy, all_preds, all_targets
+            if multi_label:
+                preds = torch.sigmoid(output)
+                all_preds.extend(preds.cpu().numpy())
+                all_targets.extend(target.cpu().numpy())
+            else:
+                _, preds = torch.max(output, 1)
+                all_preds.extend(preds.cpu().numpy())
+                all_targets.extend(target.cpu().numpy())
 
+    if multi_label:
+        # hamming = hamming_loss(all_targets, np.array(all_preds) > 0.5)
+        # accuracy = accuracy_score(all_targets, np.array(all_preds) > 0.5)
+        # return accuracy * 100, all_preds, all_targets
+        accuracy = top_2_accuracy(all_targets, np.array(all_preds)) #calculate top 2 accuracy.
+        return accuracy * 100, all_preds, all_targets
+    else:
+        correct = (np.array(all_preds) == np.array(all_targets)).sum()
+        total = len(all_targets)
+        accuracy = 100 * correct / total
+        return accuracy, all_preds, all_targets
 
 
 
@@ -122,26 +162,120 @@ def evaluate_and_save_results(model, eval_loader, index_to_label, device="cuda",
 
 
 def plot_confusion_matrix(preds, targets, index_to_label, label_type):
-    cm = confusion_matrix(targets, preds)
-    cm_percentage = cm.astype('float') / cm.sum(axis=1)[:, np.newaxis] * 100  # Convert counts to percentages
+    with open(f'./Naming_anomaly_detection/data_files/json_files/{label_type}_label_to_domain.json', 'r') as f:
+        label_to_domain = json.load(f)
+    if label_type == "arch":
+        # Randomly select 30 values for each key in label_to_domain
+        selected_labels = {}
+        for domain, labels in label_to_domain.items():
+            if len(labels) > 30:
+                selected_labels[domain] = sorted(random.sample(labels, 30))
+            else:
+                selected_labels[domain] = sorted(labels)
+
+        # Reassign label_to_domain to only include the selected labels
+        label_to_domain = selected_labels
+        selected_labels_set = {label for labels in selected_labels.values() for label in labels}
+        
+        # Create a new mapping from label to index and index to label
+        label_to_index = {label: idx for idx, label in index_to_label.items() if label in selected_labels_set}
+        index_to_label = {idx: label for idx, label in index_to_label.items() if label in selected_labels_set}
+        
+        # Filter preds and targets to only include the selected labels
+        filtered_preds = []
+        filtered_targets = []
+        for pred, target in zip(preds, targets):
+            if str(pred) in label_to_index.values() and str(target) in label_to_index.values():
+                filtered_preds.append(str(pred))
+                filtered_targets.append(str(target))
+
+        preds = np.array(filtered_preds)
+        targets = np.array(filtered_targets)
+        
+        # Filter out the labels that are not in the filtered targets
+        index_to_label = {idx: label for idx, label in index_to_label.items() if idx in set(filtered_targets)}
+        label_to_index = {label: idx for idx, label in index_to_label.items()}
+        # label_to_domain = {domain: [label for label in labels if label in label_to_index] for domain, labels in label_to_domain.items()}
+            
+    # labels_ordered = [index_to_label[str(i)] for i in range(len(index_to_label))]
+    # Order labels based on domain
+    labels_ordered = []
+    for domain, models in label_to_domain.items():
+        labels_ordered.extend(models)
+    # Ensure all labels are included
+    labels_ordered = [label for label in labels_ordered if label in index_to_label.values()]
+    # # Create a mapping from label to index
+    # label_to_index = {label: index for index, label in index_to_label.items()}
+    # print(label_to_index)
+    label_to_domain = {domain: [label for label in labels if label in labels_ordered] for domain, labels in label_to_domain.items()}
+    # print(labels_ordered)
     
-    labels = [index_to_label[i] for i in range(len(index_to_label))]
+    targets = [index_to_label[target] for target in targets]
+    preds = [index_to_label[pred] for pred in preds]
+
+    # Calculate the confusion matrix 
+    cm = confusion_matrix(targets, preds, labels=labels_ordered)
+
+    # Normalize the confusion matrix by row (i.e., by the number of samples in each true class)
+    cm_normalized = cm.astype('float') / cm.sum(axis=1)[:, np.newaxis]
     
-    fig, ax = plt.subplots(figsize=(15, 15))  # Increased figure size
-    disp = ConfusionMatrixDisplay(confusion_matrix=cm_percentage, display_labels=labels)
-    disp.plot(cmap='Blues', ax=ax, xticks_rotation='vertical', values_format='.1f')  # Rotate x-axis labels, format percentages
+    plt.figure(figsize=(10, 7))  # Adjust size as needed
+    # Plot the heatmap without annotations but with grid lines
+    ax = sns.heatmap(cm_normalized, annot=False, cmap='Blues', xticklabels=labels_ordered, yticklabels=labels_ordered, linewidths=.5)
     
-    plt.xticks(fontsize=9)  # Adjust fontsize as needed
-    plt.yticks(fontsize=9)  # Adjust fontsize as needed
-    plt.title(f'Confusion Matrix for {label_type}', fontsize=12)
-    plt.xlabel('Predicted label', fontsize=10)
-    plt.ylabel('True label', fontsize=10)
-    colorbar = ax.images[0].colorbar
-    colorbar.set_label('% of True Labels', fontsize=10)
-    colorbar.ax.yaxis.set_label_position('left')
+    # Decrease the font size of the category labels for both axes
+    if label_type == "task":
+        fontsize = 10
+    else:
+        fontsize = 5
+    ax.set_xticklabels(ax.get_xticklabels(), fontsize=fontsize)  # Adjust fontsize for x-axis labels # model_type: 5, task:10
+    ax.set_yticklabels(ax.get_yticklabels(), fontsize=fontsize)  # Adjust fontsize for y-axis labels
     
-    plt.tight_layout()  # Adjust layout to not cut off labels
-    plt.savefig(f"confusion_matrix_percentage_{label_type}.png", dpi=300)
+    # Remove the tick marks
+    ax.tick_params(axis='both', which='both', length=0)
+    
+    if label_type == "task":
+        fontsize = 10
+    else:
+        fontsize = 8
+    # Add domain labels below the x-axis
+    domain_positions = {}
+    current_position = 0
+    for domain, models in label_to_domain.items():
+        domain_positions[domain] = (current_position, current_position + len(models) - 1)
+        current_position += len(models)
+
+    for i, (domain, (start, end)) in enumerate(domain_positions.items()):
+        if domain == "Multimodal" and label_type == "task":
+            ax.text((start + end+1) / 2, len(labels_ordered)-0.1, domain, ha='center', va='bottom', fontsize=fontsize, color='black', rotation=90) # fontsize - model_type:8, task:10
+            ax.text(1.5, (start + end + 1) / 2, domain, ha='center', va='center', fontsize=fontsize, color='black')
+        else:
+            ax.text((start + end+1) / 2, len(labels_ordered)-0.1, domain, ha='center', va='bottom', fontsize=fontsize, color='black')
+            ax.text(0.5, (start + end + 1) / 2, domain, ha='center', va='center', fontsize=fontsize, color='black', rotation=90)
+        if i < len(domain_positions) - 1:
+            ax.axvline(x=end+1, color='gray', linestyle='--', linewidth=0.5)
+            ax.axhline(y=end+1, color='gray', linestyle='--', linewidth=0.5)
+    plt.tight_layout()
+    plt.savefig(f"confusion_matrix_percentage_{label_type}_final.pdf", dpi=300)
+    # cm = confusion_matrix(targets, preds)
+    # cm_percentage = cm.astype('float') / cm.sum(axis=1)[:, np.newaxis] * 100  # Convert counts to percentages
+    
+    # labels = [index_to_label[i] for i in range(len(index_to_label))]
+    
+    # fig, ax = plt.subplots(figsize=(15, 15))  # Increased figure size
+    # disp = ConfusionMatrixDisplay(confusion_matrix=cm_percentage, display_labels=labels)
+    # disp.plot(cmap='Blues', ax=ax, xticks_rotation='vertical', values_format='.1f')  # Rotate x-axis labels, format percentages
+    
+    # plt.xticks(fontsize=9)  # Adjust fontsize as needed
+    # plt.yticks(fontsize=9)  # Adjust fontsize as needed
+    # plt.title(f'Confusion Matrix for {label_type}', fontsize=12)
+    # plt.xlabel('Predicted label', fontsize=10)
+    # plt.ylabel('True label', fontsize=10)
+    # colorbar = ax.images[0].colorbar
+    # colorbar.set_label('% of True Labels', fontsize=10)
+    # colorbar.ax.yaxis.set_label_position('left')
+    
+    # plt.tight_layout()  # Adjust layout to not cut off labels
 
 
 def plot_loss(train_losses, epochs, lr, batch_size, label_type):
@@ -151,7 +285,7 @@ def plot_loss(train_losses, epochs, lr, batch_size, label_type):
     plt.ylabel('Loss')
     plt.title(f'Training Loss over Epochs | LR: {lr}, Batch Size: {batch_size}')
     plt.legend()
-    plt.savefig(f"{label_type}_train_loss_{epochs}epochs_lr{lr}_batch{batch_size}.png")
+    plt.savefig(f"{label_type}_train_loss_{epochs}epochs_lr{lr}_batch{batch_size}_final.png")
 
 
 def plot_accuracy(test_accuracies, epochs, lr, batch_size, label_type):
@@ -164,7 +298,7 @@ def plot_accuracy(test_accuracies, epochs, lr, batch_size, label_type):
     plt.title(f'Test Accuracy over Epochs | LR: {lr}, Eval Batch Size: {batch_size}')
     plt.xticks(x_vals)
     plt.legend()
-    plt.savefig(f"{label_type}_test_accuracy_{epochs}epochs_lr{lr}_batch{batch_size}.png")
+    plt.savefig(f"{label_type}_test_accuracy_{epochs}epochs_lr{lr}_batch{batch_size}_final.png")
 
 
 def run():
@@ -180,8 +314,8 @@ def run():
     # label_type = "task"
     ############################
 
-
-    vec_path = './data_cleaned.json'
+    # vec_path = './data_cleaned.json'
+    vec_path = './data_cleaned_full_arch.json'
 
     
     
@@ -230,21 +364,24 @@ def CV_run():
     from sklearn.model_selection import KFold
     ############################
     # hyperparameters
-    epochs = 40
+    epochs = 50
     lr = 1e-3
     train_batch_size = 256
     eval_batch_size = 32
 
     # label_type = "model_type"
-    label_type = "arch" # TODO: This needs a different hyperparameter setting
-    # label_type = "task"
+    label_type = "task"
+    # label_type = "arch" # TODO: This needs a different hyperparameter setting
     ############################
-    vec_path = './data_cleaned.json'
-    # vec_path = './data_cleaned_filtered.json'
-    
-    data_loader = DataLoader(vec_path)
+    if label_type == "task":
+        vec_path = 'data_cleaned.json'
+        multi_label = True
+    else:
+        vec_path = 'data.json'
+        multi_label = False
+    # data_loader = DataLoader(vec_path)
 
-    full_dataset = DARA_dataset(dict_path=vec_path, label_type=label_type)
+    full_dataset = DARA_dataset(dict_path=vec_path, label_type=label_type, multi_label=multi_label)
 
 
     index_to_label = full_dataset.get_label_mapping()
@@ -255,12 +392,6 @@ def CV_run():
     logger.info(f"Input shape: {input_shape}")
     logger.info(f"label type: {label_type}")
 
-    model = DARA_classifier(input_size=input_shape[0], output_size=num_classes).to(device)
-    
-    criterion = nn.CrossEntropyLoss()
-    optimizer = optim.Adam(model.parameters(), lr=lr)
-    # Initialize the StepLR scheduler
-    scheduler = StepLR(optimizer, step_size=30, gamma=0.1)
 
 
     kf = KFold(n_splits=5, shuffle=True, random_state=42)
@@ -290,11 +421,13 @@ def CV_run():
         model = DARA_classifier(input_size=input_shape[1], output_size=num_classes).to(device)
         optimizer = optim.Adam(model.parameters(), lr=lr)
         scheduler = StepLR(optimizer, step_size=30, gamma=0.1)
-        criterion = nn.CrossEntropyLoss()
-
-        # Train and evaluate the model on the current fold
+        if multi_label:
+            criterion = nn.BCEWithLogitsLoss()
+        else:
+            criterion = nn.CrossEntropyLoss()
         
-        train_losses, eval_accuracies, preds, targets = train_and_evaluate(model, train_loader, eval_loader, criterion, optimizer, scheduler, device=device, epochs=epochs, eval_interval=10)
+        # Train and evaluate the model on the current fold
+        train_losses, eval_accuracies, preds, targets = train_and_evaluate(model, train_loader, eval_loader, criterion, optimizer, scheduler, device=device, epochs=epochs, eval_interval=10, multi_label=multi_label)
         logger.success(f"Fold {fold}, Final Eval Accuracy: {eval_accuracies[-1]:.2f}%")
 
         all_fold_preds.extend(preds)
@@ -304,35 +437,36 @@ def CV_run():
         cumulative_eval_accuracies.append(eval_accuracies)
 
         # Optionally, save model and results per fold
-        torch.save(model.state_dict(), f'fold_{fold}_model_state_dict.pt')
-        evaluate_and_save_results(model, eval_loader, index_to_label, device=device, filepath=f'fold_{fold}_{label_type}_evaluation_results.json')
+        '''
+        torch.save(model.state_dict(), f'fold_{fold}_model_state_dict_final.pt')
+        evaluate_and_save_results(model, eval_loader, index_to_label, device=device, filepath=f'fold_{fold}_{label_type}_evaluation_results_final.json')
+        '''
 
     # After all folds are completed, calculate and log the average performance across all folds
     average_train_loss = [sum(losses) / len(losses) for losses in zip(*cumulative_train_losses)]
     average_eval_accuracy = [sum(accs) / len(accs) for accs in zip(*cumulative_eval_accuracies)]
-
+    logger.info(f"Average Eval Accuracy across all folds: {average_eval_accuracy[-1]:.2f}%")
 
     # Call plotting functions for the averages
-    plot_loss(average_train_loss, epochs, lr, train_batch_size, label_type)
-    plot_accuracy(average_eval_accuracy, epochs, lr, eval_batch_size, label_type)
+    # plot_loss(average_train_loss, epochs, lr, train_batch_size, label_type)
+    # plot_accuracy(average_eval_accuracy, epochs, lr, eval_batch_size, label_type)
 
     logger.success("5-Fold Cross Validation completed")
-
+    '''
     # Save the inputs to confusion matrix
-    np.save(f'{label_type}_all_fold_preds.npy', all_fold_preds)
-    np.save(f'{label_type}_all_fold_targets.npy', all_fold_targets)
+    np.save(f'{label_type}_all_fold_preds_final.npy', all_fold_preds)
+    np.save(f'{label_type}_all_fold_targets_final.npy', all_fold_targets)
 
     # Save index_to_label and label_type
-    with open(f'{label_type}_index_to_label.json', 'w') as f:
+    with open(f'{label_type}_index_to_label_final.json', 'w') as f:
         json.dump(index_to_label, f)
     
-    with open(f'{label_type}_label_type.json', 'w') as f:
+    with open(f'{label_type}_label_type_final.json', 'w') as f:
         json.dump(label_type, f)
 
     # # Plotting the confusion matrix for all folds
-    # plot_confusion_matrix(all_fold_preds, all_fold_targets, index_to_label, label_type)
-
-
+    plot_confusion_matrix(all_fold_preds, all_fold_targets, index_to_label, label_type)
+    '''
 if __name__ == "__main__":
     # Set random seed
     # torch.manual_seed(0)
