3fadd797f7a420d148154a6b6c603c5714dc24b8
Mingyu Kim <calixkim58@gmail.com>
2025-03-29 20:00:53 -0400

    update data extraction

--- Naming_anomaly_detection/DARA/latency_by_one.py
+++ Naming_anomaly_detection/DARA/latency_by_one.py
@@ -1,6 +1,6 @@
 import os
 import traceback
-import subprocess
+from pathlib import Path
 from dotenv import load_dotenv
 import json
 import time
@@ -8,24 +8,14 @@ import argparse
 
 load_dotenv()
 
-import numpy as np
 import torch
-import torch.nn as nn
-import torch.optim as optim
 
-from transformers import BitsAndBytesConfig
 from loguru import logger
-import matplotlib.pyplot as plt
 
-from modeling import DARA_classifier
-from dataloader import DARA_dataset
-from APTM.abstract_neural_network import AbstractNN
-from data_pre import get_model_arch_db
+from APTM.abstract_neural_network_eval import AbstractNN
 '''
 load model
-convert raw model weights -> APTM
-evaluate
-calculate throughput, latency for each model, mean std for all
+convert raw model weights -> APTM -> export APTM/Vector
 '''
 import sys
 sys.setrecursionlimit(2000) 
@@ -34,12 +24,12 @@ sys.setrecursionlimit(2000)
 os.environ['HF_HOME'] = os.getenv("HF_HOME")
 BASE_DIR = '/depot/davisjam/data/mingyu/PTM-Naming'
 PEATMOSS_PATH = os.getenv("PEATMOSS_VEC_DATA_PATH")
-device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-FAILED_REPO_PATH = "Naming_anomaly_detection/data_files/json_files/failed_final.json"
+FAILED_REPO_PATH = "Naming_anomaly_detection/data_files/json_files/failed_repos.json"
+# device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 def save_failed_model(repo_name, error_message):
     """Logs the failed model with its traceback."""
-    os.makedirs(os.path.dirname(FAILED_REPO_PATH), exist_ok=True)
+    # os.makedirs(os.path.dirname(FAILED_REPO_PATH), exist_ok=True)
     
     if os.path.exists(FAILED_REPO_PATH):
         with open(FAILED_REPO_PATH, "r", encoding="utf-8") as f:
@@ -55,8 +45,6 @@ def save_failed_model(repo_name, error_message):
 def export_vector(repo_name):
     if not PEATMOSS_PATH:
         raise ValueError("PEATMOSS_VEC_DATA_PATH environment variable is not set")
-    # if repo_name == "breadlicker45/MuseRWKV":
-    #     return
     # Create base directory if it doesn't exist
     os.makedirs(PEATMOSS_PATH, exist_ok=True)
     json_file_loc_aptm = os.path.join(PEATMOSS_PATH, "aptm")
@@ -67,156 +55,161 @@ def export_vector(repo_name):
     # latency json for each model
     aptm_output_path = os.path.join(json_file_loc_aptm, f"{repo_name}.json")
     vector_output_path = os.path.join(json_file_loc_vec, f"{repo_name}.json")
-    
 
     first_letter = repo_name[0].upper()
-    # load_in_4_bit = True
-    model_path = os.path.join(os.getenv("LOCAL_WEIGHT_PATH", ""), first_letter, repo_name)
+    local_model_path = os.path.join(os.getenv("LOCAL_WEIGHT_PATH", ""), first_letter, repo_name)
+    model_path = local_model_path if os.path.exists(local_model_path) else repo_name
     logger.info(f"Model path: {model_path}")
-    try:
-        if os.path.exists(model_path):
-            aptm, aptm_time, T_loading, total_params = AbstractNN.from_huggingface(model_path)
-        else:
-            aptm, aptm_time, T_loading, total_params = AbstractNN.from_huggingface(repo_name, cache_dir=os.getenv("HF_HOME"))
     
-        start_time = time.time()
+    try:
+        aptm, T_latency, total_params = AbstractNN.from_huggingface(model_path, cache_dir=os.getenv("HF_HOME"))
+    
         with torch.no_grad():
-            exclude_start_time = time.time()
+            start_time = time.time()
             aptm.export_aptm(aptm_output_path)
-            T_exclude = time.time() - exclude_start_time
+            T_latency['T_exclude'] += time.time() - start_time
+            export_start_time = time.time()
             aptm.export_vector(vector_output_path)
-        end_time = time.time()
+            T_export = time.time() - export_start_time
 
         logger.success("Exported vector.")
-        return vector_output_path, aptm_time, end_time - start_time - T_exclude, T_loading, total_params, T_exclude
+        T_latency['T_export'] = T_export
+        return T_latency, total_params
     
     except Exception as e:
         error_message = traceback.format_exc()
         logger.error(f"Error exporting APTM for {repo_name}: {e}")
         save_failed_model(repo_name, error_message)
         return None
-    
 
-def data_processing(data_path, label_type):
-    with open('Naming_anomaly_detection/data_files/json_files/all_keys.json', 'r') as f:
-        all_keys = json.load(f)
-    # logger.info(f"Processing data from {data_path}")
-    data = {}
-    with open(data_path, 'r') as f:
-        root, file = os.path.split(data_path)
-        model_name = "/".join([root.split("/")[-1], file.removesuffix(".json")])
-        # logger.info(f"Processing model {model_name}")
-        vecs = json.load(f)
-        processed_vecs = {}
-        # Process each type of vector
-        for vec_type in ['l', 'p', 'd']:
-            vec_data = vecs.get(vec_type, {})
-            processed_vec = []
-            # Convert each key in the specific vector to its index in the all_keys enumeration
-            for key in all_keys[vec_type]:
-                processed_vec.append(vec_data.get(key, 0))
-            processed_vecs[vec_type] = processed_vec
-        # data[model_name] = processed_vecs
-        l_tensor = torch.tensor(processed_vecs['l'], dtype=torch.float).unsqueeze(0) 
-        p_tensor = (1 * torch.tensor(processed_vecs['p'], dtype=torch.float)).unsqueeze(0)
-        vec = torch.cat((l_tensor, p_tensor), dim=1)  # Concatenate along the new dimension
+def load_json(file_path):
+    """Helper function to load a JSON file."""
+    path = Path(file_path)
+    if path.exists():
+        with path.open("r") as f:
+            return json.load(f)
+    return {}
 
-    return vec
+def save_json(file_path, data):
+    """Helper function to save a JSON file."""
+    path = Path(file_path)
+    path.parent.mkdir(parents=True, exist_ok=True)
+    with path.open("w") as f:
+        json.dump(data, f)
+
+# def data_processing(data_path, label_type):
+#     with open('Naming_anomaly_detection/data_files/json_files/all_keys.json', 'r') as f:
+#         all_keys = json.load(f)
+#     # logger.info(f"Processing data from {data_path}")
+#     data = {}
+#     with open(data_path, 'r') as f:
+#         root, file = os.path.split(data_path)
+#         model_name = "/".join([root.split("/")[-1], file.removesuffix(".json")])
+#         # logger.info(f"Processing model {model_name}")
+#         vecs = json.load(f)
+#         processed_vecs = {}
+#         # Process each type of vector
+#         for vec_type in ['l', 'p', 'd']:
+#             vec_data = vecs.get(vec_type, {})
+#             processed_vec = []
+#             # Convert each key in the specific vector to its index in the all_keys enumeration
+#             for key in all_keys[vec_type]:
+#                 processed_vec.append(vec_data.get(key, 0))
+#             processed_vecs[vec_type] = processed_vec
+#         # data[model_name] = processed_vecs
+#         l_tensor = torch.tensor(processed_vecs['l'], dtype=torch.float).unsqueeze(0) 
+#         p_tensor = (1 * torch.tensor(processed_vecs['p'], dtype=torch.float)).unsqueeze(0)
+#         vec = torch.cat((l_tensor, p_tensor), dim=1)  # Concatenate along the new dimension
+
+#     return vec
         
 def main():
     parser = argparse.ArgumentParser(description="Evaluate a PTM, latency and FLOPs.")
     parser.add_argument("--repo", type=str, required=True, help="Repository name for the model (e.g., 'hf-internal-testing/tiny-random-LEDForConditionalGeneration')")
-    parser.add_argument("--label_type", type=str, required=True, help="label type for evaluation (e.g., 'model_type', 'task', 'arch')")
-    parser.add_argument("--fold", type=int, required=True, help="fold number for the model")
     args = parser.parse_args()
     
+    json_file_loc_latency = Path(PEATMOSS_PATH) / "latency.json"
+    json_file_loc_params = Path(PEATMOSS_PATH) / "params.json"
+
+    # Load existing JSON data
+    latency = load_json(json_file_loc_latency)
+    params = load_json(json_file_loc_params)
+    
+    if latency.get(args.repo, -1) != -1:
+        logger.info(f"Latency for {args.repo} already exists.")
+        return
+    
+    logger.info(f"Exporting APTM for {args.repo}")
+    sample_start_time = time.time()
+    
     try:
-        vec_path = './data_cleaned_full_arch.json'
-        full_dataset = DARA_dataset(dict_path=vec_path, label_type=args.label_type)
-
-        index_to_label = full_dataset.get_label_mapping()
-        num_classes = full_dataset.get_num_classes()
-        input_shape = full_dataset.get_data_shape()
-        
-        json_file_loc_latency = os.path.join(PEATMOSS_PATH, "latency.json")
-        os.makedirs(os.path.dirname(json_file_loc_latency), exist_ok=True)
-        
-        json_file_loc_flop = os.path.join(PEATMOSS_PATH, "flop.json")
-        os.makedirs(os.path.dirname(json_file_loc_flop), exist_ok=True)
-
-        if os.path.exists(json_file_loc_latency):
-            with open(json_file_loc_latency, 'r') as f:
-                latency = json.load(f)
-        else:
-            latency = {}
-        if args.repo in latency and latency[args.repo] != -1:
-            logger.info(f"Latency for {args.repo} already exists.")
-            exit()
-        # create vector
-        if os.path.exists(json_file_loc_flop):
-            with open(json_file_loc_flop, 'r') as f:
-                params = json.load(f)
-        else:
-            params = {}
-        
-        logger.info(f"Exporting APTM for {args.repo}")
-        sample_start_time = time.time()
         result = export_vector(args.repo)
-            
+        
         # failed to export vector
-        latency[args.repo] = -1
         if not result:
-            with open(json_file_loc_latency, 'w') as f:
-                json.dump(latency, f)
-        output_path, T_aptm, T_export, T_loading, total_params, T_exclude = result
+            latency[args.repo] = -1
+            params[args.repo] = -1
+            save_json(json_file_loc_latency, latency)
+            save_json(json_file_loc_params, params)
+            return
         
-        exclude_start_time = time.time()
-        params[args.repo] = total_params
-        with open(json_file_loc_flop, "w") as f:
-            json.dump(params, f)
-        logger.success(f"Total # of Params for {args.repo} calculated and saved.")
-        T_exclude += time.time() - exclude_start_time
-            
-        # data processing
-        process_start_time = time.time()
-        data = data_processing(data_path=output_path, label_type=args.label_type)
-        T_process = time.time() - process_start_time
+        T_latency, total_params = result
+        T_latency['T_total'] = time.time() - sample_start_time - T_latency['T_exclude']
+        del T_latency['T_exclude']
         
-        # Initialize the model for the current fold
-        prediction_start_time = time.time()
-        model = DARA_classifier(input_size=input_shape[1], output_size=num_classes)
-        PATH = f'{BASE_DIR}/fold_{args.fold}_model_state_dict_final.pt'
-        model.load_state_dict(torch.load(PATH))
-        model = model.to(device)
-        model.eval()
-        data = data.to(device)
-        output = model(data)
-        _, predicted = torch.max(output, 1)
-        T_prediction = time.time() - prediction_start_time
-        
-        # logger.info(f'index_to_label: {index_to_label}, predict: {predicted.item()}, target: {target[i]}')
-        logger.info(f'predict: {index_to_label[predicted.item()]}') #, target: {args.index_to_label[target[i].item()]}')
-        T_total = time.time() - sample_start_time - T_exclude
-        T_overhead = T_total - (T_aptm + T_export + T_process + T_prediction + T_loading)
-        # latency calculation
-        latency[args.repo] = {
-            'aptm': T_aptm,
-            'export': T_export,
-            'process': T_process,
-            'prediction': T_prediction,
-            'loading': T_loading,
-            'overhead': T_overhead,
-            'total': T_total
-        }
-        with open(json_file_loc_latency, 'w') as f:
-            json.dump(latency, f)
+        latency[args.repo] = T_latency
+        save_json(json_file_loc_latency, latency)
         logger.success(f"Latency for {args.repo} calculated and saved.")
         
+        params[args.repo] = total_params
+        save_json(json_file_loc_params, params)
+        logger.success(f"Total # of Params for {args.repo} calculated and saved.")
+
     except Exception as e:
         error_message = traceback.format_exc()
         logger.error(f"Error exporting APTM for {args.repo}: {e}")
         save_failed_model(args.repo, error_message)
-        return None
+            
+    #     # data processing
+    #     process_start_time = time.time()
+    #     data = data_processing(data_path=output_path, label_type=args.label_type)
+    #     T_process = time.time() - process_start_time
+        
+    #     # Initialize the model for the current fold
+    #     prediction_start_time = time.time()
+    #     model = DARA_classifier(input_size=input_shape[1], output_size=num_classes)
+    #     PATH = f'{BASE_DIR}/fold_{args.fold}_model_state_dict_final.pt'
+    #     model.load_state_dict(torch.load(PATH))
+    #     model = model.to(device)
+    #     model.eval()
+    #     data = data.to(device)
+    #     output = model(data)
+    #     _, predicted = torch.max(output, 1)
+    #     T_prediction = time.time() - prediction_start_time
+        
+    #     # logger.info(f'index_to_label: {index_to_label}, predict: {predicted.item()}, target: {target[i]}')
+    #     logger.info(f'predict: {index_to_label[predicted.item()]}') #, target: {args.index_to_label[target[i].item()]}')
+        # T_total = time.time() - sample_start_time - T_exclude
+    #     T_overhead = T_total - (T_aptm + T_export + T_process + T_prediction + T_loading)
+        # latency calculation
+        # latency[args.repo] = {
+        #     'aptm': T_aptm,
+            # 'export': T_export,
+            # 'process': T_process,
+            # 'prediction': T_prediction,
+            # 'loading': T_loading,
+            # 'overhead': T_overhead,
+            # 'total': T_total
+        # }
+        # with open(json_file_loc_latency, 'w') as f:
+        #     json.dump(latency, f)
+        # logger.success(f"Latency for {args.repo} calculated and saved.")
+        
+    # except Exception as e:
+    #     error_message = traceback.format_exc()
+    #     logger.error(f"Error exporting APTM for {args.repo}: {e}")
+    #     save_failed_model(args.repo, error_message)
+    #     return None
 
 if __name__ == "__main__":
     main()
--- Naming_anomaly_detection/DARA/launcher.py
+++ Naming_anomaly_detection/DARA/launcher.py
@@ -18,7 +18,7 @@ BASE_DIR = '/depot/davisjam/data/mingyu/PTM-Naming'
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 PEATMOSS_PATH = os.getenv("PEATMOSS_VEC_DATA_PATH")
-json_file_loc_flop = os.path.join(PEATMOSS_PATH, "flop.json")
+json_file_loc_flop = os.path.join(PEATMOSS_PATH, "params.json")
 if os.path.exists(json_file_loc_flop):
     with open(json_file_loc_flop, 'r') as f:
         params = json.load(f)
@@ -32,179 +32,27 @@ def set_seed(seed=42):
     torch.backends.cudnn.deterministic = True
     torch.backends.cudnn.benchmark = False
     
-def run_all_models(model_list, label_type, fold):
-    for repo in model_list:
+def run_all_models(repos):
+    for repo in repos:
         if repo in params:
             print(f"{repo} already exists, skipping...")
             continue
-        exit()
-        print(f"Starting evaluation for model: {repo}")
-        p = subprocess.Popen(["python", "Naming_anomaly_detection/DARA/latency_by_one.py", "--repo", repo, "--label_type", label_type, "--fold", str(fold)])    
-        p.wait()
-
-def CV_run():
-    
-    # ############################
-    # # hyperparameters
-    # epochs = 40
-    # lr = 1e-3
-    # train_batch_size = 256
-    eval_batch_size = 32
-
-    label_type = "model_type"
-    # label_type = "arch" # TODO: This needs a different hyperparameter setting
-    # label_type = "task"
-    ############################
-    vec_path = './data_cleaned.json'
-    # vec_path = './data_cleaned_filtered.json'
-    
-    # data_loader = DataLoader(vec_path)
-
-    full_dataset = DARA_dataset(dict_path=vec_path, label_type=label_type)
-
-    index_to_label = full_dataset.get_label_mapping()
-    num_classes = full_dataset.get_num_classes()
-    input_shape = full_dataset.get_data_shape()
-    
-    logger.info(f"Number of classes: {num_classes}")
-    logger.info(f"Input shape: {input_shape}")
-    logger.info(f"label type: {label_type}")
-    
-    kf = KFold(n_splits=5, shuffle=True, random_state=42)
-
-    fold = 0  # Counter for current fold
-
-    for train_index, eval_index in kf.split(full_dataset):
-        fold += 1
-        logger.info(f"Starting fold {fold}")
-
-        # Create datasets for the current fold (simulating train-test split)
-        eval_subset = torch.utils.data.Subset(full_dataset, eval_index)
-
-        # Initialize DataLoaders for the current fold
-        eval_loader = DataLoader(eval_subset, batch_size=eval_batch_size, num_workers=0, pin_memory=True)
         
-        # # Initialize the model for the current fold
-        # model = DARA_classifier(input_size=input_shape[1], output_size=num_classes)
-        # PATH = f'{BASE_DIR}/fold_{fold}_model_state_dict.pt'
-        # model.load_state_dict(torch.load(PATH))
-        # model = model.to(device)
-        # model.eval()
+        start_time = time.time()
+        logger.info(f"Starting evaluation for model: {repo}")
+        result = subprocess.run(["python", "Naming_anomaly_detection/DARA/latency_by_one.py", "--repo", repo])    
+        end_time = time.time()
+        print(f"Finished evaluation for model: {repo} in {end_time - start_time} seconds.\n", result.stdout)
 
-        # json_file_loc_latency = os.path.join(PEATMOSS_PATH, "latency.json")
-        # os.makedirs(os.path.dirname(json_file_loc_latency), exist_ok=True)
-        
-        # json_file_loc_flop = os.path.join(PEATMOSS_PATH, "flop.json")
-        # os.makedirs(os.path.dirname(json_file_loc_flop), exist_ok=True)
-
-
-        with torch.no_grad():
-            start_time = time.time()
-            for _, target, names in eval_loader:
-                run_all_models(names, label_type, fold)
-            #     for i, name in enumerate(names):
-            #         if os.path.exists(json_file_loc_latency):
-            #             with open(json_file_loc_latency, 'r') as f:
-            #                 latency = json.load(f)
-            #         else:
-            #             latency = {}
-                        
-            #         # if os.path.exists(json_file_loc_flop):
-            #         #     with open(json_file_loc_flop, 'r') as f:
-            #         #         flop = json.load(f)
-            #         # else:
-            #         #     flop = {}
-            #         print(torch.cuda.memory_summary())
-            #         if name in latency and latency[name] != -1:
-            #             logger.info(f"Latency for {name} already exists.")
-            #             continue
-                    
-            #         # create vector
-            #         logger.info(f"Exporting APTM for {name}")
-            #         sample_start_time = time.time()
-            #         result = export_vector(name)
-            #         # flops, macs, params = calculateFlop(name)
-                    
-            #         # failed to export vector
-            #         latency[name] = -1
-            #         if not result:
-            #             with open(json_file_loc_latency, 'w') as f:
-            #                 json.dump(latency, f)
-            #             continue
-            #         output_path, T_aptm, T_export, complexity = result
-            #         total_aptm_time += T_aptm
-            #         total_export_time += T_export
-            #         output_path = result
-            #         # Code for calculating FLOP
-            #         # flops, macs, params = complexity
-            #         # if flops != -1:
-            #         #     flop[name] = {"flops": flops, "macs": macs, "params": params}
-            #         # else:
-            #         #     flop[name] = {"flops": -1, "macs": -1, "params": -1}
-            #         # with open(json_file_loc_flop, "w") as f:
-            #         #     json.dump(flop, f)
-                        
-            #         # data processing
-            #         process_start_time = time.time()
-            #         data = data_processing(data_path=output_path, label_type=label_type)
-            #         T_process = time.time() - process_start_time
-            #         total_process_time += T_process
-                    
-            #         data = data.to(device)
-            #         inference_start_time = time.time()
-            #         output = model(data)
-                    
-
-            #         _, predicted = torch.max(output, 1)
-            #         T_inference = time.time() - inference_start_time
-            #         total_inference_time += T_inference
-            #         # logger.info(f'index_to_label: {index_to_label}, predict: {predicted.item()}, target: {target[i]}')
-            #         logger.info(f'predict: {index_to_label[predicted.item()]}, target: {index_to_label[target[i].item()]}')
-            #         T_total = time.time() - sample_start_time
-            #         T_overhead = T_total - (T_aptm + T_export + T_process + T_inference)
-            #         total_overhead_time += T_overhead
-            #         # latency calculation
-            #         latency[name] = {
-            #             'aptm': T_aptm,
-            #             'export': T_export,
-            #             'process': T_process,
-            #             'inference': T_inference,
-            #             'overhead': T_overhead,
-            #             'total': T_total
-            #         }
-            #         with open(json_file_loc_latency, 'w') as f:
-            #             json.dump(latency, f)
-            #         logger.success(f"Latency for {name} calculated and saved.")
-                    
-            #         total_samples += 1
-                    
-            # total_time += time.time() - start_time
-            # logger.success(f"Fold {fold} completed.")
-            
-    
-    # logger.success(f"Total time: {total_time:.4f} sec")
-    # logger.success(f"    - export time: {total_export_time:.4f} sec")
-    # logger.success(f"    - process time: {total_process_time:.4f} sec")
-    # logger.success(f"    - inference time: {total_inference_time:.4f} sec\n")
-    
-    # logger.success(f"Average latency: {total_time / total_samples:.4f} sec\n")
-    
-    # logger.success(f"Throughput: {total_samples / total_time:.2f} samples/sec")
-    # logger.success(f"    - export time: {total_samples / total_export_time:.4f} samples/sec")
-    # logger.success(f"    - process time: {total_samples / total_process_time:.4f} samples/sec")
-    # logger.success(f"    - inference time: {total_samples / total_inference_time:.4f} samples/sec\n")
-    
-    # logger.success(f"Overhead: {total_overhead_time / total_time:.2f} sec")
-
-    # Call plotting functions for the averages
-    # plot_loss(average_train_loss, epochs, lr, train_batch_size, label_type)
-    # plot_accuracy(average_eval_accuracy, epochs, lr, eval_batch_size, label_type)
-
-    logger.success("5-Fold Cross Validation completed")
     
 if __name__ == "__main__":
     set_seed(0)
-    # CV_run()
+    with open("Naming_anomaly_detection/data_files/json_files/temp_selected_peatmoss_repos.json", "r") as f:
+        repos = json.load(f)
+
+        with torch.no_grad():
+            run_all_models(repos)
+
     
     # label_type = "model_type"
     # fold = 1
@@ -227,8 +75,8 @@ if __name__ == "__main__":
     # repo = "arham061/speecht5_finetuned_voxpopuli_nl"
     # repo = "reciprocate/gpt2-tiny"
     # repo = "hf-internal-testing/tiny-random-BertLMHeadModel"
-    repo = "tanaya-b/urdu_sms_hingbert"
     # repo = "hf-internal-testing/tiny-random-resnet"
-    label_type = "model_type"
-    fold = 1
-    subprocess.Popen(["python", "Naming_anomaly_detection/DARA/latency_by_one_temp.py", "--repo", repo, "--label_type", label_type, "--fold", str(fold)])    
+    # repo = "sazzad-sit/whisper-small-bn-cv13-gf"
+    # label_type = "model_type"
+    # fold = 1
+    # subprocess.Popen(["python", "Naming_anomaly_detection/DARA/latency_by_one_temp.py", "--repo", repo, "--label_type", label_type, "--fold", str(fold)])    
