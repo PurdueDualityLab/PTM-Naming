2f580e8d12429d96bdcb3a0ad9a50453007441f0
Mingyu Kim <calixkim58@gmail.com>
2025-04-17 15:11:32 -0400

    transformer update

--- Naming_anomaly_detection/DARA/transformer/loss.py
+++ Naming_anomaly_detection/DARA/transformer/loss.py
@@ -6,98 +6,14 @@ def contrastive_loss(device, loss_fn, tau, lambd, label_type):
     if loss_fn == 'CL':
         return CL(device, tau, lambd)
     elif loss_fn == 'CLCE':
-        return OptimizedCLCE(device, tau, lambd, label_type)
+        return SupConCE(device, tau, lambd, label_type)
         # return CLCE(device, tau, lambd, label_type)
     elif loss_fn == 'FoCL':
         return FoCL(device, tau)
     else:
         raise ValueError(f"Invalid loss type: {loss_fn}")
 
-# train CL and head separately
-class CL(nn.Module):
-    def __init__(self, device, tau, lambd):
-        super().__init__()
-        self.device = device
-        self.tau = tau
-        self.lambd = lambd
-        
-    def forward(self, layer_embeds, y_true):
-        loss_temp = torch.zeros(len(layer_embeds), len(layer_embeds)*2-1, device=self.device, dtype=torch.float)
-        for i in range(len(layer_embeds)):
-            indice = 1
-            pos = True
-            for j in range(len(layer_embeds)):
-                if i == j:  continue
-                if y_true[i] == y_true[j] and pos:
-                    loss_temp[i][0] = (F.cosine_similarity(layer_embeds[i].view(1,-1), layer_embeds[j].view(1,-1)) + 1) * 0.5 * self.tau
-                    pos = False
-                elif y_true[i] != y_true[j]: 
-                    loss_temp[i][indice] = (F.cosine_similarity(layer_embeds[i].view(1,-1), layer_embeds[j].view(1,-1)) + 1) * 0.5 * self.tau
-                    indice += 1
-    
-        CL_loss = -nn.LogSoftmax(dim=1)(loss_temp)
-        CL_loss = torch.sum(CL_loss, dim=0)[0]
-        CL_loss /= len(layer_embeds)
-        
-        loss = CL_loss
-        return loss
-
-class FocalLoss(nn.Module):
-    def __init__(self, alpha=1, gamma=2, reduction='mean'):
-        super(FocalLoss, self).__init__()
-        self.alpha = alpha
-        self.gamma = gamma
-        self.reduction = reduction
-
-    def forward(self, inputs, targets):
-        BCE_loss = F.cross_entropy(inputs, targets, reduction='none')
-        pt = torch.exp(-BCE_loss)
-        F_loss = self.alpha * (1 - pt) ** self.gamma * BCE_loss
-
-        if self.reduction == 'mean':
-            return F_loss.mean()
-        elif self.reduction == 'sum':
-            return F_loss.sum()
-        else:
-            return F_loss
-# one label at a time
-class CLCE(nn.Module):
-    def __init__(self, device, tau, lambd, label_type):
-        super().__init__()
-        self.device = device
-        self.tau = tau
-        self.lambd = lambd    #tweak
-        # self.gamma = 2
-        self.label_type = label_type
-        print(f"lambda value for CLCE loss: {lambd}")
-        
-    def forward(self, layer_embeds, logits, y_true):
-        loss_temp = torch.zeros(len(layer_embeds), len(layer_embeds)*2-1, device=self.device, dtype=torch.float)
-        for i in range(len(layer_embeds)):
-            indice = 1
-            pos = True
-            for j in range(len(layer_embeds)):
-                if i == j:  continue
-                if y_true[i] == y_true[j] and pos:
-                    loss_temp[i][0] = (F.cosine_similarity(layer_embeds[i].view(1,-1), layer_embeds[j].view(1,-1)) + 1) * 0.5 * self.tau
-                    pos = False
-                elif y_true[i] != y_true[j]:
-                    loss_temp[i][indice] = (F.cosine_similarity(layer_embeds[i].view(1,-1), layer_embeds[j].view(1,-1)) + 1) * 0.5 * self.tau
-                    indice += 1         
-
-        CL_loss = -nn.LogSoftmax(dim=1)(loss_temp)
-        CL_loss = torch.sum(CL_loss, dim=0)[0]
-        CL_loss /= len(layer_embeds)
-
-        if self.label_type == 'task':
-            CE_loss = nn.BCEWithLogitsLoss()(logits, y_true.float())
-        else:
-            CE_loss = nn.CrossEntropyLoss()(logits, y_true)
-        loss = self.lambd * CL_loss + (1 - self.lambd) * CE_loss
-
-        return loss
-    
-class OptimizedCLCE(nn.Module):
+class SupConCE(nn.Module):
     def __init__(self, device, tau, lambd, label_type):
         super().__init__()
         self.device = device
@@ -180,6 +96,92 @@ class OptimizedCLCE(nn.Module):
         
     
         return loss
+
+# train CL and head separately
+class CL(nn.Module):
+    def __init__(self, device, tau, lambd):
+        super().__init__()
+        self.device = device
+        self.tau = tau
+        self.lambd = lambd
+        
+    def forward(self, layer_embeds, y_true):
+        loss_temp = torch.zeros(len(layer_embeds), len(layer_embeds)*2-1, device=self.device, dtype=torch.float)
+        for i in range(len(layer_embeds)):
+            indice = 1
+            pos = True
+            for j in range(len(layer_embeds)):
+                if i == j:  continue
+                if y_true[i] == y_true[j] and pos:
+                    loss_temp[i][0] = (F.cosine_similarity(layer_embeds[i].view(1,-1), layer_embeds[j].view(1,-1)) + 1) * 0.5 * self.tau
+                    pos = False
+                elif y_true[i] != y_true[j]: 
+                    loss_temp[i][indice] = (F.cosine_similarity(layer_embeds[i].view(1,-1), layer_embeds[j].view(1,-1)) + 1) * 0.5 * self.tau
+                    indice += 1
+    
+        CL_loss = -nn.LogSoftmax(dim=1)(loss_temp)
+        CL_loss = torch.sum(CL_loss, dim=0)[0]
+        CL_loss /= len(layer_embeds)
+        
+        loss = CL_loss
+        return loss
+
+class FocalLoss(nn.Module):
+    def __init__(self, alpha=1, gamma=2, reduction='mean'):
+        super(FocalLoss, self).__init__()
+        self.alpha = alpha
+        self.gamma = gamma
+        self.reduction = reduction
+
+    def forward(self, inputs, targets):
+        BCE_loss = F.cross_entropy(inputs, targets, reduction='none')
+        pt = torch.exp(-BCE_loss)
+        F_loss = self.alpha * (1 - pt) ** self.gamma * BCE_loss
+
+        if self.reduction == 'mean':
+            return F_loss.mean()
+        elif self.reduction == 'sum':
+            return F_loss.sum()
+        else:
+            return F_loss
+
+# one label at a time
+class CLCE(nn.Module):
+    def __init__(self, device, tau, lambd, label_type):
+        super().__init__()
+        self.device = device
+        self.tau = tau
+        self.lambd = lambd    #tweak
+        # self.gamma = 2
+        self.label_type = label_type
+        print(f"lambda value for CLCE loss: {lambd}")
+        
+    def forward(self, layer_embeds, logits, y_true):
+        loss_temp = torch.zeros(len(layer_embeds), len(layer_embeds)*2-1, device=self.device, dtype=torch.float)
+        for i in range(len(layer_embeds)):
+            indice = 1
+            pos = True
+            for j in range(len(layer_embeds)):
+                if i == j:  continue
+                if y_true[i] == y_true[j] and pos:
+                    loss_temp[i][0] = (F.cosine_similarity(layer_embeds[i].view(1,-1), layer_embeds[j].view(1,-1)) + 1) * 0.5 * self.tau
+                    pos = False
+                elif y_true[i] != y_true[j]:
+                    loss_temp[i][indice] = (F.cosine_similarity(layer_embeds[i].view(1,-1), layer_embeds[j].view(1,-1)) + 1) * 0.5 * self.tau
+                    indice += 1         
+
+        CL_loss = -nn.LogSoftmax(dim=1)(loss_temp)
+        CL_loss = torch.sum(CL_loss, dim=0)[0]
+        CL_loss /= len(layer_embeds)
+
+        if self.label_type == 'task':
+            CE_loss = nn.BCEWithLogitsLoss()(logits, y_true.float())
+        else:
+            CE_loss = nn.CrossEntropyLoss()(logits, y_true)
+        loss = self.lambd * CL_loss + (1 - self.lambd) * CE_loss
+
+        return loss
+    
     
 class FoCL(nn.Module):
     def __init__(self, device, tau):
